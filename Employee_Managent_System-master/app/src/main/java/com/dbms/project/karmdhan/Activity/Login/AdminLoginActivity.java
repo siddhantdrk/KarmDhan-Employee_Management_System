4a1b7e7cacab437ae98e441200557712f0450dc7
Siddhant Khobragade <siddhantdrk@gmail.com>
2021-04-26 00:37:21 +0530

    Admin account Added

--- Employee_Managent_System-master/app/src/main/java/com/dbms/project/karmdhan/Activity/Login/AdminLoginActivity.java
+++ Employee_Managent_System-master/app/src/main/java/com/dbms/project/karmdhan/Activity/Login/AdminLoginActivity.java
@@ -1,5 +1,6 @@
 package com.dbms.project.karmdhan.Activity.Login;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,19 +8,23 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.dbms.project.karmdhan.Activity.MainActivity;
+import com.dbms.project.karmdhan.DB.AdminOperations;
+import com.dbms.project.karmdhan.Model.Admin;
 import com.dbms.project.karmdhan.R;
+import com.dbms.project.karmdhan.Storage.SharedPreferenceManager;
 import com.dbms.project.karmdhan.databinding.ActivityAdminLoginBinding;
-import com.dbms.project.karmdhan.databinding.ActivityEmployeeLoginBinding;
-import com.google.android.material.textfield.TextInputEditText;
 
 public class AdminLoginActivity extends AppCompatActivity {
-    private ActivityAdminLoginBinding binding ;
-    TextInputEditText userNameEdt,passwordEdt;
+    private ActivityAdminLoginBinding binding;
+    private AdminOperations adminOperations;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityAdminLoginBinding.inflate(LayoutInflater.from(this));
         setContentView(binding.getRoot());
+        adminOperations = new AdminOperations(this);
+        adminOperations.addAdmin(new Admin("12345678", "12345678"));
         binding.loginBtn.setOnClickListener(this::onClick);
     }
 
@@ -30,12 +35,26 @@ public class AdminLoginActivity extends AppCompatActivity {
     }
 
     private void login() {
-        String name = binding.userIdEdt.getText().toString().trim();
+        String userId = binding.userIdEdt.getText().toString().trim();
         String password = binding.passwordEdt.getText().toString().trim();
 
-        if(!name.isEmpty()){
-            if(!password.isEmpty()){
-
+        if (!userId.isEmpty()) {
+            if (!adminOperations.checkAdminIDExist(userId)) {
+                Toast.makeText(this, "User ID doesn't exist", Toast.LENGTH_SHORT).show();
+            }
+            if (!password.isEmpty()) {
+                Admin admin = new Admin(userId, password);
+                if (adminOperations.checkLoginCredentials(admin)) {
+                    Toast.makeText(this, "Logged In successfully", Toast.LENGTH_SHORT).show();
+                    SharedPreferenceManager.getInstance(this).saveToken(userId);
+                    Intent intent = new Intent(this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP |
+                            Intent.FLAG_ACTIVITY_CLEAR_TASK |
+                            Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(this, "Wrong credentials !! Please fill the correct details", Toast.LENGTH_SHORT).show();
+                }
             }else{
                 Toast.makeText(AdminLoginActivity.this, "password is empty", Toast.LENGTH_SHORT).show();
             }
--- Employee_Managent_System-master/app/src/main/java/com/dbms/project/karmdhan/Activity/WelcomeActivity.java
+++ Employee_Managent_System-master/app/src/main/java/com/dbms/project/karmdhan/Activity/WelcomeActivity.java
@@ -1,15 +1,18 @@
 package com.dbms.project.karmdhan.Activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dbms.project.karmdhan.Activity.Login.AdminLoginActivity;
 import com.dbms.project.karmdhan.Activity.Login.EmployeeLoginActivity;
 import com.dbms.project.karmdhan.R;
+import com.dbms.project.karmdhan.Storage.SharedPreferenceManager;
 import com.dbms.project.karmdhan.databinding.ActivityWelcomeBinding;
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -19,18 +22,34 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityWelcomeBinding.inflate(LayoutInflater.from(this));
-        setContentView(binding.getRoot());
+        checkIfUserExists();
         binding.proceedEmployeeBtn.setOnClickListener(this::onClick);
         binding.proceedAdminBtn.setOnClickListener(this::onClick);
     }
 
+    private void checkIfUserExists() {
+        if (SharedPreferenceManager.getInstance(this).getToken() != null) {
+            if (SharedPreferenceManager.getInstance(this).isAdmin())
+                startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+            else
+                Toast.makeText(this, "Employee", Toast.LENGTH_SHORT).show();
+            //startActivity(new Intent(WelcomeActivity.this, GuestDashBoardActivity.class));
+            finish();
+        } else {
+            setContentView(binding.getRoot());
+        }
+    }
+
+    @SuppressLint("NonConstantResourceId")
     private void onClick(View view) {
         switch (view.getId()) {
             case R.id.proceed__employee_btn:
+                SharedPreferenceManager.getInstance(this).setAdmin(false);
                 Intent intentEmpLogin = new Intent(this, EmployeeLoginActivity.class);
                 startActivity(intentEmpLogin);
                 break;
             case R.id.proceed_admin_btn:
+                SharedPreferenceManager.getInstance(this).setAdmin(true);
                 Intent intentAdminLogin = new Intent(this, AdminLoginActivity.class);
                 startActivity(intentAdminLogin);
                 break;
